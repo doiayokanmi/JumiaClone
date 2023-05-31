6d09c862d2b68432a8826b6a12a11185fd816650
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-05-31 12:51:55 +0100

    sign up JS initiated but not yet queried

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-const signInBtn = document.getElementById('signIn');
+// Go To Sign In Page
+const signIn = () => {
+  window.location.href = 'signin.html';
+};
 
-signInBtn.addEventListener('click', () => {
-    window.location.href = 'signin.html';
-})
+// Sign Up Query
+const signUpBtn = document.getElementById('signUpBtn');
+
+signUpBtn.addEventListener('click', () => {
+  const allUser = [];
+  const signUpName = document.getElementById('signUpName').value;
+  const signUpEmail = document.getElementById('signUpEmail').value;
+  const signUpPassword = document.getElementById('signUpPassword').value; 
+  const signUpCPassword = document.getElementById('signUpCPassword').value; 
+  
+  const objUser = {
+    name: signUpName,
+    email: signUpEmail,
+    pass: signUpPassword,
+    confirmPass: signUpCPassword
+  };
+
+  allUser.push(objUser);
+
+  console.log(allUser);
+});
