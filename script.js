ff67d595501603832ce14c000f0b6c7e0db54fd2
Ibrahim Damilola Dhikirullahi <oloyedammy@gmail.com>
2023-06-01 10:16:19 +0100

    comment add to js functions

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Go To Sign In Page
+// Function to navigate to the sign-in page
 const signIn = () => {
   window.location.href = 'signin.html';
 };
 
+// Get the sign-up modal element
 var signUpModal = document.getElementById('signUpModal')
 
-// Sign Up Query
-const signUpBtn = document.getElementById('signUpBtn');
+// Check if there are any existing users in local storage, otherwise initialize an empty array
 var allUser;
 var LSallUser = localStorage.getItem('allUser');
 if (LSallUser) {
@@ -15,23 +15,29 @@ if (LSallUser) {
   allUser = [];
 }
 
+// Function to clear sign-up input fields
 const emptySignUpInput = () => {
-      signUpName.value = "";
-      signUpEmail.value = "";
-      signUpPassword.value = "";
-      signUpCPassword.value = "";
+  signUpName.value = "";
+  signUpEmail.value = "";
+  signUpPassword.value = "";
+  signUpCPassword.value = "";
 }
 
+// Event listener for the sign-up button
 signUpBtn.addEventListener('click', () => {
   const signUpName = document.getElementById('signUpName');
   const signUpEmail = document.getElementById('signUpEmail');
-  const signUpPassword = document.getElementById('signUpPassword'); 
-  const signUpCPassword = document.getElementById('signUpCPassword'); 
+  const signUpPassword = document.getElementById('signUpPassword');
+  const signUpCPassword = document.getElementById('signUpCPassword');
 
-  if (signUpName.value != "" && signUpEmail.value != "" && signUpPassword.value !="") {
-    
+  // Check if all required fields are filled
+  if (signUpName.value != "" && signUpEmail.value != "" && signUpPassword.value != "") {
+
+    // Check if the password matches the confirm password
     if (signUpPassword.value == signUpCPassword.value) {
-      errMsgDis()
+      errMsgDis() // Clear any previous error message
+
+      // Create an object to represent the new user
       const objUser = {
         name: signUpName.value,
         email: signUpEmail.value,
@@ -40,6 +46,8 @@ signUpBtn.addEventListener('click', () => {
 
       let SUpUserFound = 0;
       let signUpEmailAd = document.getElementById('signUpEmail')
+
+      // Check if the user already exists
       for (let index = 0; index < allUser.length; index++) {
         if (allUser[index].email == signUpEmailAd.value) {
           SUpUserFound = 1
@@ -48,49 +56,54 @@ signUpBtn.addEventListener('click', () => {
       }
 
       if (SUpUserFound == 1) {
-        errMsg.innerHTML = "User already exist, click on sign in link to login to your account"
+        // User already exists, display error message and clear input fields
+        errMsg.innerHTML = "User already exists. Click on the sign-in link to log in to your account"
+        emptySignUpInput()
+        errMsgTimeOut()
+      } else {
+        // Add the new user to the array
+        allUser.push(objUser);
+
+        // Store the updated user array in local storage
+        let LSAllUser = JSON.stringify(allUser);
+        localStorage.setItem('allUser', LSAllUser);
+
+        // Display success message and clear input fields
+        errMsg.innerHTML = `Congratulations! You are now a customer of Jumia.com.ng <br /> Click on the sign-in link below to access your account <br /> You will be redirected to the login page soon <br />`
+        signUpModal.innerHTML += `Congratulations! You are now a customer of Jumia.com.ng <br /> You will be redirected to the login page soon <br /> <img src='src/Infinity-1s-83px.svg'>`
+        signUpModal.style.display = "flex"
+        errMsg.style.color = "green"
         emptySignUpInput()
         errMsgTimeOut()
 
-      } else {
-        allUser.push(objUser);
+        // Redirect to the sign-in page after a delay
+        setTimeout(() => {
+          window.location.href = 'signin.html'
+        }, 10000)
 
-      let LSAllUser = JSON.stringify(allUser);
-      localStorage.setItem('allUser', LSAllUser);
-      errMsg.innerHTML = `Congratulation!!! You are now a customer of Jumia.com.ng <br /> Click on sign in link below to access your account <br /> You will be redirected to login page soon <br />`
-      signUpModal.innerHTML += `Congratulation!!! You are now a customer of Jumia.com.ng <br /> You will be redirected to login page soon <br /> <img src='src/Infinity-1s-83px.svg'>`
-      signUpModal.style.display = "flex"
-      errMsg.style.color = "green"
-      emptySignUpInput()
-      errMsgTimeOut()
-
-      setTimeout(() => {
-        window.location.href = 'signin.html'
-      }, 10000)
-
-      console.log(allUser);
+        console.log(allUser);
       }
 
     } else {
-      errMsg.innerHTML = "Password do not match"
+      // Passwords do not match, display error message and focus on the password field
+      errMsg.innerHTML = "Passwords do not match"
       signUpPassword.focus()
       errMsgTimeOut()
     }
 
   } else {
-    errMsg.innerHTML = "* All field required"
+    // Not all required fields are filled, display error message
+    errMsg.innerHTML = "* All fields are required"
     errMsgTimeOut()
-
   }
-
 });
 
+// Function to clear error message
 const errMsgDis = () => {
   errMsg.innerHTML = ""
 }
 
+// Function to clear error message after a timeout
 const errMsgTimeOut = () => {
-
   setTimeout(errMsgDis, 10000)
-} 
-
+}
